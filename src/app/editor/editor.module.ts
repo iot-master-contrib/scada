18fd948e283b640c09d4a004335e22cb98070520
jason <jason@zgwit.com>
2023-04-09 10:26:23 +0800

    工具条

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -3,11 +3,32 @@ import {CommonModule} from '@angular/common';
 
 import {EditorRoutingModule} from './editor-routing.module';
 import {EditorComponent} from "./editor.component";
-import { LibraryComponent } from './library/library.component';
-import { CanvasComponent } from './canvas/canvas.component';
-import { PropertyComponent } from './property/property.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-
+import {LibraryComponent} from './library/library.component';
+import {CanvasComponent} from './canvas/canvas.component';
+import {PropertyComponent} from './property/property.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzDividerModule} from "ng-zorro-antd/divider";
+import {
+    SaveOutline,
+    ExportOutline,
+    UndoOutline,
+    RedoOutline,
+    ScissorOutline,
+    CopyOutline,
+    SnippetsOutline,
+    DeleteOutline,
+    AlignLeftOutline,
+    AlignCenterOutline,
+    AlignRightOutline,
+    VerticalAlignTopOutline,
+    VerticalAlignMiddleOutline,
+    VerticalAlignBottomOutline,
+    UpOutline,
+    DownOutline,
+    GroupOutline,
+    UngroupOutline
+} from '@ant-design/icons-angular/icons';
 
 @NgModule({
     declarations: [
@@ -19,7 +40,11 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     ],
     imports: [
         CommonModule,
-        EditorRoutingModule
+        EditorRoutingModule,
+        NzIconModule.forChild([
+            SaveOutline, ExportOutline, UndoOutline, RedoOutline, ScissorOutline, CopyOutline, SnippetsOutline, DeleteOutline, AlignLeftOutline, AlignCenterOutline, AlignRightOutline, VerticalAlignTopOutline, VerticalAlignMiddleOutline, VerticalAlignBottomOutline, UpOutline, DownOutline, GroupOutline, UngroupOutline
+        ]),
+        NzDividerModule
     ]
 })
 export class EditorModule {
