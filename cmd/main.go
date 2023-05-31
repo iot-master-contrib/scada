d34eb9339cdca268fb8f5bf5609c5204f60ae7d5
Jason <jason@zgwit.com>
2023-06-01 00:11:56 +0800

    1

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"github.com/iot-master-contrib/scada"
+	"github.com/iot-master-contrib/scada/config"
 	"github.com/zgwit/iot-master/v3/pkg/banner"
 	"github.com/zgwit/iot-master/v3/pkg/build"
 	"github.com/zgwit/iot-master/v3/pkg/db"
 	"github.com/zgwit/iot-master/v3/pkg/log"
+	"github.com/zgwit/iot-master/v3/pkg/mqtt"
 	"github.com/zgwit/iot-master/v3/pkg/web"
 )
 
 func main() {
 	banner.Print("iot-master-plugin:scada")
 	build.Print()
+
+	config.Load()
 
 	err := log.Open()
 	if err != nil {
@@ -20,6 +24,12 @@ func main() {
 
 	//加载数据库
 	err = db.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//MQTT总线
+	err = mqtt.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
