bb1949ed113f32d10a7f8a54b1ea476f18ca4e7c
Jason <jason@zgwit.com>
2023-06-02 10:00:05 +0800

    bugfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NZ_I18N} from 'ng-zorro-antd/i18n';
 import {zh_CN} from 'ng-zorro-antd/i18n';
-import {registerLocaleData} from '@angular/common';
+import {APP_BASE_HREF, registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
