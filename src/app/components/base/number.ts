7d2ec9f125aa9f3f6e0eec3788efdc21595e6c0f
Jason <jason@zgwit.com>
2023-06-13 15:19:57 +0800

    数据绑定OK

--- src/app/components/base/number.ts
+++ src/app/components/base/number.ts
@@ -1,34 +1,15 @@
 import {HmiComponent} from "../../hmi";
 import {createStrokeProperties} from "../properties";
-import {ObjectExt} from "@antv/x6";
 
 export const BaseNumber: HmiComponent = {
     name: '数值', id: 'number', icon: '/app/scada/assets/number.svg', type: "shape",
-    extends: {
-        inherit: "rect",
-        propHooks(meta:any){
-            console.log("number propHooks", meta)
-            const {data, ...others} = meta
-            if (data) {
-                let fixed = data.fixed || 0
-                //ObjectExt.getByPath(meta, "data/fixed")
-                ObjectExt.setByPath(meta, "attrs/text/text", data.value?.toFixed(fixed))
-            }
-            return meta
-        }
-    },
+    extends: {inherit: "rect"},
     meta: {
         width: 100,
         height: 30,
         attrs: {
-            rect: {
-                fill: 'none',
-                stroke: 'none',
-                strokeWidth: 2
-            },
-            text: {
-                text: '123'
-            },
+            rect: {fill: 'none', stroke: 'none'},
+            text: {text: 'number'},
         },
     },
     properties: [
@@ -37,5 +18,11 @@ export const BaseNumber: HmiComponent = {
     ],
     bindings: [
         {name: 'value', label: "数值", default: 12.06},
-    ]
+    ],
+    hooks: {
+        value(value, cell) {
+            let fixed = cell.getPropByPath("data/fixed") || 0
+            cell.setPropByPath("attrs/text/text", value?.toFixed(fixed))
+        }
+    },
 }
--- src/app/hmi.ts
+++ src/app/hmi.ts
@@ -32,6 +32,9 @@ export declare interface HmiComponent {
     //开放的数据绑定
     bindings?: HmiComponentBinding[]
 
+    //数据绑定的钩子
+    hooks?: { [name: string]: ((value: any, cell: Cell) => void) | string }
+
     //开放的事件
     events?: HmiComponentEvent[]
 
@@ -55,7 +58,6 @@ export declare interface HmiComponentEvent {
     name: string
     label: string
 }
-
 
 
 export declare interface HmiCollection {
