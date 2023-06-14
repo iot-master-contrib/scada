957dfacba21b8908b71ef601e5ae522a2ae6cebb
Jason <jason@zgwit.com>
2023-06-14 10:14:30 +0800

    小优化

--- src/hmi/base/triangle.ts
+++ src/hmi/base/triangle.ts
@@ -1,9 +1,12 @@
 import { HmiComponent } from "../hmi";
 import {TriangleSvg} from "./triangle.embed";
+import {createStrokeProperties} from "../properties";
 
 export const BaseTriangle: HmiComponent = {
     name: '三角形', id: 'triangle', svg: TriangleSvg, type: "shape",
     extends: { inherit: "path" },
     meta: { width: 100, height: 40, path: 'M20.5 18.5H4.5L12.5 5.5L20.5 18.5Z' },
-    properties: [],
+    properties: [
+        ...createStrokeProperties("path")
+    ],
 }
