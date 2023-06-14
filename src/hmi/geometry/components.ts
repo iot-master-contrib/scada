6516adf0d5093f325acbd6d929df6e57e0ed7497
Jason <jason@zgwit.com>
2023-06-14 11:15:23 +0800

    补充几个几何图形

--- src/hmi/geometry/components.ts
+++ src/hmi/geometry/components.ts
@@ -13,14 +13,32 @@ import {PentagramSvg} from "./pentagram.embed";
 import {PentagramMeta} from "./pentagram.meta";
 import {RhombusSvg} from "./rhombus.embed";
 import {RhombusMeta} from "./rhombus.meta";
+import {ArrowLeftSvg} from "./arrow-left.embed";
+import {ArrowLeftMeta} from "./arrow-left.meta";
+import {ArrowRightSvg} from "./arrow-right.embed";
+import {ArrowRightMeta} from "./arrow-right.meta";
+import {ArrowLeftBlockSvg} from "./arrow-left-block.embed";
+import {ArrowLeftBlockMeta} from "./arrow-left-block.meta";
+import {ArrowRightBlockSvg} from "./arrow-right-block.embed";
+import {ArrowRightBlockMeta} from "./arrow-right-block.meta";
+import {LeftSvg} from "./left.embed";
+import {LeftMeta} from "./left.meta";
+import {RightSvg} from "./right.embed";
+import {RightMeta} from "./right.meta";
 
 const images: HmiSvgComponent[] = [
-    {id: 'cube', name: '', svg: CubeSvg, ...CubeMeta},
-    {id: 'cylinder', name: '', svg: CylinderSvg, ...CylinderMeta},
-    {id: 'forbidden', name: '', svg: ForbiddenSvg, ...ForbiddenMeta},
-    {id: 'pentagon', name: '', svg: PentagonSvg, ...PentagonMeta},
-    {id: 'pentagram', name: '', svg: PentagramSvg, ...PentagramMeta},
-    {id: 'rhombus', name: '', svg: RhombusSvg, ...RhombusMeta},
+    {id: 'cube', svg: CubeSvg, ...CubeMeta},
+    {id: 'cylinder', svg: CylinderSvg, ...CylinderMeta},
+    {id: 'forbidden', svg: ForbiddenSvg, ...ForbiddenMeta},
+    {id: 'pentagon', svg: PentagonSvg, ...PentagonMeta},
+    {id: 'pentagram', svg: PentagramSvg, ...PentagramMeta},
+    {id: 'rhombus', svg: RhombusSvg, ...RhombusMeta},
+    {id: 'left', svg: LeftSvg, ...LeftMeta},
+    {id: 'right', svg: RightSvg, ...RightMeta},
+    {id: 'arrow-left', svg: ArrowLeftSvg, ...ArrowLeftMeta},
+    {id: 'arrow-right', svg: ArrowRightSvg, ...ArrowRightMeta},
+    {id: 'arrow-left-block', svg: ArrowLeftBlockSvg, ...ArrowLeftBlockMeta},
+    {id: 'arrow-right-block', svg: ArrowRightBlockSvg, ...ArrowRightBlockMeta},
 ]
 
 export const GeometryComponents: HmiCollection = {
